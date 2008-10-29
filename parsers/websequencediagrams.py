193c693b9738752e332048303f559a628df4d0bd
andre.roberge <andre.roberge@a9de93d0-a543-11dd-a5cd-697bfd0a0cef>
2008-10-29 02:16:44 +0000

    Simplified websequence.

--- parsers/websequencediagrams.py
+++ parsers/websequencediagrams.py
@@ -7,38 +7,35 @@ import re
 from _parser import BaseParser
 import src.svg as svg
 
-_patterns = {
-    # anything is declared to be good; we let the website worry about it.
-    'any': re.compile("^(.*?)$")
-}
-
 def register_docpicture_parser(register_parser):
     register_parser(WebSequence)
 
 class WebSequence(BaseParser):
     '''a parser creating web sequence diagrams'''
     def __init__(self):
-        self.patterns = _patterns
         self.directive_name = 'websequence'
 
     def get_svg_defs(self):
         '''No svg diagrams produced by this parser.'''
         return svg.Comment("ignore me")
 
-    def draw(self, lines):
-        '''Insert appropriate reference so that graphics can be
-        loaded from website'''
+    def create_drawing(self, lines):
+        '''Parses all received lines of code.
+
+           NORMALLY, If errors are found, returns a list of line with errors and
+           an empty string, otherwise returns None (for no error) and a list of
+           lines parsed to extract the relevant information.
+
+           HOWEVER, here we assume that all lines are valid lines as they are
+           passed to a remote server which should deal with them...
+        '''
         div = svg.XmlElement("div", wsd_style="modern-blue")
         div.attributes['class'] = "wsd"
-        relevant_text = []
-        for line in lines:
-            for items in line[1::2]:
-                relevant_text.append(items[0])
-        text = "\n".join(relevant_text)
+        text = "\n".join(lines)
         div.append(svg.XmlElement("pre", text=text))
         main_div = svg.XmlElement("div")
         main_div.append(div)
         main_div.append(svg.XmlElement("script",
                             type="text/javascript",
                             src="http://www.websequencediagrams.com/service.js"))
-        return main_div
+        return None, main_div
