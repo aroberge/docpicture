2f7d7292d1559e08ba436ca5e819cfeffb74961f
andre.roberge <andre.roberge@a9de93d0-a543-11dd-a5cd-697bfd0a0cef>
2008-10-29 15:57:41 +0000

    Completed implementation of available styles for websequencediagrams.py; started on comprehensive example.

--- trunk/parsers/websequencediagrams.py
+++ trunk/parsers/websequencediagrams.py
@@ -9,6 +9,14 @@ import src.svg as svg
 
 def register_docpicture_parser(register_parser):
     register_parser(WebSequence)
+    register_parser(WebSequenceEarth)
+    register_parser(WebSequenceModernBlue)
+    register_parser(WebSequenceMscgen)
+    register_parser(WebSequenceOmegapple)
+    register_parser(WebSequenceQsd)
+    register_parser(WebSequenceRose)
+    register_parser(WebSequenceRoundgreen)
+    register_parser(WebSequenceNapkin)
 
 class WebSequence(BaseParser):
     '''a parser creating web sequence diagrams'''
@@ -41,50 +49,51 @@ class WebSequence(BaseParser):
                             src="http://www.websequencediagrams.com/service.js"))
         return None, main_div
 
-class WebSequenceEarth(Websequence):
+class WebSequenceEarth(WebSequence):
     '''a parser creating web sequence diagrams; earth style'''
     def __init__(self):
         self.directive_name = 'uml_sequence:earth'
         self.style = "earth"
 
-class WebSequenceModernBlue(Websequence):
+class WebSequenceModernBlue(WebSequence):
     '''a parser creating web sequence diagrams; modern-blue style'''
     def __init__(self):
         self.directive_name = 'uml_sequence:modern-blue'
         self.style = "modern-blue"
 
-class WebSequenceMscgen(Websequence):
+class WebSequenceMscgen(WebSequence):
     '''a parser creating web sequence diagrams; mscgen style'''
     def __init__(self):
         self.directive_name = 'uml_sequence:mscgen'
         self.style = "mscgen"
 
-class WebSequenceOmegapple(Websequence):
+class WebSequenceOmegapple(WebSequence):
     '''a parser creating web sequence diagrams; omegapple style'''
     def __init__(self):
         self.directive_name = 'uml_sequence:omegapple'
         self.style = "omegapple"
 
-class WebSequenceQsd(Websequence):
+class WebSequenceQsd(WebSequence):
     '''a parser creating web sequence diagrams; qsd style'''
     def __init__(self):
         self.directive_name = 'uml_sequence:qsd'
         self.style = "qsd"
 
-class WebSequenceRose(Websequence):
+class WebSequenceRose(WebSequence):
     '''a parser creating web sequence diagrams; rose style'''
     def __init__(self):
         self.directive_name = 'uml_sequence:rose'
         self.style = "rose"
 
-"""
-    * default
-    * earth
-    * modern-blue
-    * mscgen
-    * omegapple
-    * qsd
-    * rose
-    * roundgreen
-    * napkin
-"""
+class WebSequenceRoundgreen(WebSequence):
+    '''a parser creating web sequence diagrams; roundgreen style'''
+    def __init__(self):
+        self.directive_name = 'uml_sequence:roundgreen'
+        self.style = "roundgreen"
+
+class WebSequenceNapkin(WebSequence):
+    '''a parser creating web sequence diagrams; napkin style'''
+    def __init__(self):
+        self.directive_name = 'uml_sequence:napkin'
+        self.style = "napkin"
+
