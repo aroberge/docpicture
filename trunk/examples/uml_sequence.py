001b1262d6c9f444f01f1a3f93f431ca24488565
andre.roberge <andre.roberge@a9de93d0-a543-11dd-a5cd-697bfd0a0cef>
2008-10-29 11:25:00 +0000

    Created examples directory and separate uml_sequence example.

---
+++ trunk/examples/uml_sequence.py
@@ -0,0 +1,14 @@
+"""
+Just an empty module used as a test for uml sequence diagrams.
+
+http://www.websequencediagrams.com
+
+Aside: just for a quick test, we include another type of object.
+..docpicture:: uml_sequence
+  Alice->Bob: Authentication Request
+  Bob-->Alice: Authentication Response
+
+
+
+"""
+print "Read the source."
